7e7d94dbe42d431585e9cd21d4ede52b6db67228
Qin Huqiang <huxiangjs@foxmail.com>
2023-11-21 01:03:09 +0800

    Updated example for testing unpack functionality
    Signed-off-by: Qin Huqiang <huxiangjs@foxmail.com>

--- examples/main.c
+++ examples/main.c
@@ -24,53 +24,129 @@
 
 #include <stdio.h>
 #include <gitt_ssh.h>
+#include <gitt_unpack.h>
 
-#define PRIVKEY_PATH	"/home/huxiang/.ssh/id_ed25519"
+// #define PRIVKEY_PATH	"/home/huxiang/.ssh/id_ed25519"
 
-int main(int args, char *argv[])
+// int main(int args, char *argv[])
+// {
+// 	struct gitt_ssh* ssh = gitt_ssh_alloc();
+// 	int err;
+// 	char buffer[4096];
+// 	FILE *file;
+
+// 	if (!ssh)
+// 		return -1;
+
+// 	/* Read key file */
+// 	file = fopen(PRIVKEY_PATH, "rb");
+// 	if (!file)
+// 		goto out0;
+// 	err = fread(buffer, 1, sizeof(buffer) - 1, file);
+// 	if (err <= 0) {
+// 		fclose(file);
+// 		return -1;
+// 	}
+// 	buffer[err] = '\0';
+// 	printf("%s\n", buffer);
+// 	fclose(file);
+
+// 	/* Connect */
+// 	err = gitt_ssh_connect(ssh, "git@github.com:huxiangjs/git_things.git",
+// 			       "git-receive-pack", buffer);
+// 	if (err)
+// 		goto out0;
+
+// 	/* Read */
+// 	err = gitt_ssh_read(ssh, buffer, sizeof(buffer));
+// 	// while (err > 0) {
+// 	if (err > 0) {
+// 		printf("%.*s\n", err, buffer);
+// 		// err = ssh_channel_read(ssh->channel, buffer, sizeof(buffer), 0);
+// 	}
+
+// 	gitt_ssh_disconnect(ssh);
+
+// 	printf("Done\n");
+
+// 	return 0;
+
+// out0:
+// 	gitt_ssh_free(ssh);
+// 	return -1;
+// }
+
+static void gitt_obj_dump_callback(struct gitt_obj *obj)
 {
-	struct gitt_ssh* ssh = gitt_ssh_alloc();
+	printf("%s: type:%s, size:%d\n", __func__, gitt_obj_types[obj->type], obj->size);
+	if (obj->type == 1)
+		printf("%.*s\n", obj->size, obj->data);
+}
+
+static void gitt_unpack_work_callback(struct gitt_unpack *unpack)
+{
+	printf("%s: version:%u, number of objects:%u\n", __func__, unpack->version, unpack->number);
+}
+
+static void gitt_unpack_verify_callback(bool pass, struct gitt_sha1 *sha1)
+{
+	char sha1_hex[41];
+	int ret;
+
+	printf("%s: pass:%s\n", __func__, pass ? "true" : "false");
+
+	ret = gitt_sha1_hexdigest(sha1, sha1_hex);
+	if (!ret)
+		printf("SHA-1: %s\n", sha1_hex);
+}
+
+static void test_unpack(uint8_t *buf, uint16_t len)
+{
+	struct gitt_unpack unpack = {0};
+	uint8_t buffer[4096];
+	uint16_t count;
 	int err;
-	char buffer[4096];
+
+	unpack.buf = buffer;
+	unpack.buf_len = sizeof(buffer);
+	unpack.obj_dump = gitt_obj_dump_callback;
+	unpack.work = gitt_unpack_work_callback;
+	unpack.verify = gitt_unpack_verify_callback;
+	gitt_unpack_init(&unpack);
+
+	count = 0;
+	while (count < len) {
+		err = gitt_unpack_update(&unpack, buf + count, 128);
+		if (err)
+			break;
+		count += 128;
+	}
+}
+
+static int test_unpack_from_file(void)
+{
 	FILE *file;
+	int err;
+	uint8_t buffer[40960];
 
-	if (!ssh)
-		return -1;
-
-	/* Read key file */
-	file = fopen(PRIVKEY_PATH, "rb");
+	file = fopen("../../git_impl/.git/objects/pack/pack-c901ded5d19bb4affeb9f8336653c12637f93cc7.pack", "rb");
 	if (!file)
-		goto out0;
+		return -1;
 	err = fread(buffer, 1, sizeof(buffer) - 1, file);
 	if (err <= 0) {
 		fclose(file);
 		return -1;
 	}
-	buffer[err] = '\0';
-	printf("%s\n", buffer);
-	fclose(file);
+	printf("file size: %dbyte\n", err);
 
-	/* Connect */
-	err = gitt_ssh_connect(ssh, "git@github.com:huxiangjs/git_things.git",
-			       "git-receive-pack", buffer);
-	if (err)
-		goto out0;
-
-	/* Read */
-	err = gitt_ssh_read(ssh, buffer, sizeof(buffer));
-	// while (err > 0) {
-	if (err > 0) {
-		printf("%.*s\n", err, buffer);
-		// err = ssh_channel_read(ssh->channel, buffer, sizeof(buffer), 0);
-	}
-
-	gitt_ssh_disconnect(ssh);
-
-	printf("Done\n");
+	test_unpack(buffer, (uint16_t)err);
 
 	return 0;
+}
 
-out0:
-	gitt_ssh_free(ssh);
-	return -1;
+int main(int args, char *argv[])
+{
+	test_unpack_from_file();
+
+	return 0;
 }
