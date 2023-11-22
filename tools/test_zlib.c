01fd2af8ba12afbf194b3a59ae08d5ef5c89ad08
Hoozz <huxiangjs@foxmail.com>
2023-11-22 21:29:32 +0800

    Modify zlib test program to prepare for future use of stream decompress
    Signed-off-by: Hoozz <huxiangjs@foxmail.com>

--- tools/test_zlib.c
+++ tools/test_zlib.c
@@ -26,37 +26,145 @@
 #include <string.h>
 #include <zlib.h>
 
-int main(int args, char *argv[])
+/* Check the return value of zlib functions */
+static void zlib_check_ret(int ret) {
+	switch (ret) {
+	case Z_OK:
+		fprintf(stdout, "OK\n");
+		break;
+	case Z_STREAM_END:
+		fprintf(stdout, "Stream end\n");
+		break;
+	case Z_NEED_DICT:
+		fprintf(stdout, "Need dictionary\n");
+		break;
+	case Z_ERRNO:
+		fprintf(stderr, "Error\n");
+		break;
+	case Z_STREAM_ERROR:
+		fprintf(stderr, "Stream error\n");
+		break;
+	case Z_DATA_ERROR:
+		fprintf(stderr, "Data error\n");
+		break;
+	case Z_MEM_ERROR:
+		fprintf(stderr, "Memory error\n");
+		break;
+	case Z_BUF_ERROR:
+		fprintf(stderr, "Buffer error\n");
+		break;
+	case Z_VERSION_ERROR:
+		fprintf(stderr, "Version error\n");
+		break;
+	default:
+		fprintf(stderr, "Unknown error\n");
+	}
+}
+
+/* Compress a buffer using deflate */
+static int zlib_compress(unsigned char *in, size_t in_size,
+			 unsigned char *out, size_t *out_size)
 {
-	Bytef tmp1[64] = "This is a string for testing!";
-	Bytef tmp2[64] = {0};
-	uLong tmp1_size;
-	uLong tmp2_size;
-	uLong i;
-	int retval;
+	int ret;
+	z_stream strm;
+	size_t have;
 
-	printf("zlib version: %s\n", zlibVersion());
+	/* Initialize the stream structure */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	strm.avail_in = 0;
+	strm.next_in = Z_NULL;
 
-	/* Compress */
-	tmp1_size = (uLong)strlen(tmp1);
-	tmp2_size = sizeof(tmp2);
-	retval = compress(tmp2, &tmp2_size, tmp1, tmp1_size);
-	if (!retval) {
-		for (i  = 0; i < tmp2_size; i++)
-			printf("%02x ", tmp2[i]);
-		printf("(%lubyte)\n", tmp2_size);
-	} else {
-		printf("compress error: %d\n", retval);
+	/* Use the default compression level */
+	ret = deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+	zlib_check_ret(ret);
+	if (ret != Z_OK)
+		return ret;
+
+	/* Compress the input buffer in chunks */
+	strm.avail_in = in_size;
+	strm.next_in = in;
+	strm.avail_out = *out_size;
+	strm.next_out = out;
+
+	ret = deflate(&strm, Z_FINISH);
+	zlib_check_ret(ret);
+	if (ret == Z_STREAM_ERROR)
+		return ret;
+
+	/* Number of bytes in the output buffer */
+	have = *out_size - strm.avail_out;
+	fprintf(stdout, "Compressed %lu bytes into %lu bytes\n", in_size, have);
+
+	/* Clean up */
+	deflateEnd (&strm);
+	*out_size = have;
+
+	return Z_OK;
+}
+
+/* Decompress a buffer using inflate */
+int zlib_decompress(unsigned char *in, size_t in_size,
+			unsigned char *out, size_t *out_size)
+{
+	int ret;
+	z_stream strm;
+	size_t have;
+
+	/* Initialize the stream structure */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	strm.avail_in = 0;
+	strm.next_in = Z_NULL;
+
+	ret = inflateInit(&strm);
+	zlib_check_ret(ret);
+	if (ret != Z_OK)
+		return ret;
+
+	/* Decompress the input buffer in chunks */
+	strm.avail_in = in_size;
+	strm.next_in = in;
+	strm.avail_out = *out_size;
+	strm.next_out = out;
+
+	ret = inflate(&strm, Z_NO_FLUSH);
+	zlib_check_ret(ret);
+	switch (ret) {
+	case Z_NEED_DICT:
+	case Z_DATA_ERROR:
+	case Z_MEM_ERROR:
+		inflateEnd(&strm);
+		return ret;
 	}
 
-	/* Uncompress */
-	tmp1_size = sizeof(tmp1);
-	memset(tmp1, 0, tmp1_size);
-	retval = uncompress(tmp1, &tmp1_size, tmp2, tmp2_size);
-	if (!retval)
-		printf("%s (%lubyte)\n", (char *)tmp1, tmp1_size);
-	else
-		printf("uncompress error: %d\n", retval);
+	/* Number of bytes in the output buffer */
+	have = *out_size - strm.avail_out;
+	fprintf(stdout, "Decompressed %lu bytes into %lu bytes\n", in_size, have);
+
+	/* Clean up */
+	inflateEnd (&strm);
+	*out_size = have;
+
+	return Z_OK;
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned char in[4096];
+	unsigned char out[4096];
+	size_t in_size = sizeof(in);
+	size_t out_size = sizeof(out);
+	int i;
+
+	/* Fill the input buffer with some data */
+	for (i = 0; i < in_size; i++)
+		in [i] = i % 256;
+
+	zlib_compress(in, in_size, out, &out_size);
+	zlib_decompress(out, out_size, in, &in_size);
 
 	return 0;
 }
